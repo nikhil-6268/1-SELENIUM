2c93e4b6fa2dc96489c1f192cbe30047f99eadbf
Akshauhini <138785878+nikhil-6268@users.noreply.github.com>
2024-10-17 22:13:54 +0530

    Add files via upload

---
+++ WebDriverMgrClass.java
@@ -0,0 +1,34 @@
+package Demo.WebDriverMgr;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverMgrClass {
+
+	public static void main(String[] args) {
+//		chrome driver
+//		 System.setProperty("webdriver.chrome.driver","D:\\New folder\\chromedriver-win64\\chromedriver.exe");
+//		 WebDriver driver = new ChromeDriver();
+//		 Microsoft edge driver
+//		 System.setProperty("webdriver.edge.driver","D:\\New folder\\edgedriver_win64\\msedgedriver.exe");	
+// 		 WebDriver driver = new EdgeDriver();
+		
+		//No need of line no. 12 to 17 we can directly use or start from line no.20
+//		 LAUNCHING ALL BROWSER AT ONCE IN A PARALLEL FORMAT BY USING WEB DRIVER MANAGER
+//		 WebDriverManager.edgedriver().setup(); // for Microsoft driver
+//		 WebDriver driver = new EdgeDriver();
+//		 WebDriverManager.firefoxdriver();
+//		 WebDriver driver = new firefoxDriver();
+		 WebDriverManager.chromedriver().setup(); // chrome for chrome driver
+		 WebDriver driver = new ChromeDriver();	 // use webdriver and new browser driver
+		 driver.get("https://www.google.com/");
+		 driver.switchTo().newWindow(WindowType.TAB);
+		 driver.get("https://www.youtube.com/");
+		 
+	}
+
+}
